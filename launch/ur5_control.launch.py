31e5792f7eff86bbc37d930d7493247bf916acde
Wellington Araujo <wellington.araujo@fieb.org.br>
2025-05-13 15:47:31 -0300

    Trajectory arm controller is working

--- launch/ur5_control.launch.py
+++ launch/ur5_control.launch.py
@@ -4,6 +4,9 @@ from launch.substitutions import Command, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 from launch.substitutions import FindExecutable
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
 
 
 def generate_launch_description():
@@ -14,7 +17,7 @@ def generate_launch_description():
         PathJoinSubstitution([
             FindPackageShare("ur_description"),
             "urdf",
-            "ur.urdf.xacro"
+            "ur_mocked.urdf.xacro"
         ]),
         " ",
         "name:=ur",
@@ -22,6 +25,14 @@ def generate_launch_description():
         "ur_type:=ur5e",
         " ",
         "prefix:=''",
+    ])
+
+    # ToDO: Create a xacro that contains the model and control
+
+    robot_controllers = PathJoinSubstitution([
+        FindPackageShare("ur5_arm"),
+        "config",
+        "ros2_controllers.yaml",
     ])
 
     # RViz
@@ -51,12 +62,42 @@ def generate_launch_description():
         executable="joint_state_publisher_gui",
     )
 
+    ros2_control_node = Node(
+		package="controller_manager",
+		executable="ros2_control_node",
+		parameters=[{"robot_description": robot_description}, robot_controllers],
+		output="both",
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["ur5_arm_controller", "--param-file", robot_controllers],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+        output="screen",
+    )
+
+    delay_joint_state_broadcaster_after_robot_controller_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=robot_controller_spawner,
+            on_exit=[joint_state_broadcaster_spawner],
+        )
+    )
+
     return LaunchDescription([
-        rviz_node,
         robot_state_pub_node,
-        joint_state_publisher_node
+        # joint_state_publisher_node,
         # static_tf,
         # run_move_group_node,
-        # ros2_control_node,
+        ros2_control_node,
+        robot_controller_spawner,
+        # joint_state_broadcaster_spawner,
+        rviz_node,
+        delay_joint_state_broadcaster_after_robot_controller_spawner
     ])
     # + load_controllers
