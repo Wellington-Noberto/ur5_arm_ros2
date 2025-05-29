1afea89bafadc035f00db3fbab273af6cbecd0aa
Wellington Araujo <wellington.araujo@fieb.org.br>
2025-05-29 16:41:39 -0300

    Model is imported

--- launch/ur5_control.launch.py
+++ launch/ur5_control.launch.py
@@ -3,6 +3,7 @@ from launch import LaunchDescription
 from launch.substitutions import Command, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import FindExecutable
 from launch.actions import DeclareLaunchArgument, RegisterEventHandler
 from launch.conditions import IfCondition
@@ -11,13 +12,13 @@ from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
     # Get URDF via xacro
-    robot_description = Command([
+    robot_description = ParameterValue(Command([
         PathJoinSubstitution([FindExecutable(name="xacro")]),
         " ",
         PathJoinSubstitution([
             FindPackageShare("ur5_arm"),
             "urdf",
-            "ur5.urdf.xacro"
+            "ur5e_joint_limited_robot.urdf.xacro"
         ]),
         " ",
         "name:=ur",
@@ -25,7 +26,8 @@ def generate_launch_description():
         "ur_type:=ur5e",
         " ",
         "tf_prefix:=''",
-    ])
+    ]),
+    value_type=str)
 
     # ToDO: Create a xacro that contains the model and control
 
@@ -62,12 +64,12 @@ def generate_launch_description():
         executable="joint_state_publisher_gui",
     )
 
-    ros2_control_node = Node(
-		package="controller_manager",
-		executable="ros2_control_node",
-		parameters=[{"robot_description": robot_description}, robot_controllers],
-		output="both",
-    )
+    # ros2_control_node = Node(
+	# 	package="controller_manager",
+	# 	executable="ros2_control_node",
+	# 	parameters=[{"robot_description": robot_description}, robot_controllers],
+	# 	output="both",
+    # )
 
     arm_controller_spawner = Node(
         package="controller_manager",
@@ -97,13 +99,13 @@ def generate_launch_description():
 
     return LaunchDescription([
         robot_state_pub_node,
-        # joint_state_publisher_node,
+        joint_state_publisher_node,
         # static_tf,
         # run_move_group_node,
-        ros2_control_node,
-        arm_controller_spawner,
-        gripper_controller_spawner,
+        # ros2_control_node,
+        # arm_controller_spawner,
+        # gripper_controller_spawner,
         rviz_node,
-        delay_joint_state_broadcaster_after_robot_controller_spawner
+        # delay_joint_state_broadcaster_after_robot_controller_spawner
     ])
     # + load_controllers
